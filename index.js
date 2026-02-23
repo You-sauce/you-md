906480ab580289d8f264599e1fcbff8d6bafb6d5
You-sauce <yousauce274@gmail.com>
2026-02-23 15:22:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  * Creator: popkid
  * Improvements: auto-reconnect, error guards, keep-alive, safe plugins
  */
-
+if (global.started) return;
+global.started = true;
 console.clear()
 console.log("📳 Starting YOU-MD...")
 
